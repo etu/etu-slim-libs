b27bf3414b315c7e24bb738ca625e73a015397bd
Elis Hirwing <elis@hirwing.se>
2023-01-17 22:18:44 +0100

    env: Upgrade PHP to 8.2

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? (import <nixpkgs> {}) }:
 
 let
-  php' = pkgs.php80;
+  php' = pkgs.php82;
 
 in pkgs.mkShell {
   buildInputs = [
@@ -12,30 +12,16 @@ in pkgs.mkShell {
     php'
     php'.packages.composer
 
-    # Install style check tools (pick newer versions than on nixos-unstable
-    # for better PHP8.0 compatibility, this is fixed on master).
-    (php'.packages.phpcbf.overrideAttrs(oa: {
-      version = "3.6.0";
-      src = pkgs.fetchurl {
-        url = "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.6.0/phpcbf.phar";
-        sha256 = "04wb1imm4934mpy2hxcmqh4cn7md1vwmfii39p6mby809325b5z1";
-      };
-    }))
-    (php'.packages.phpcs.overrideAttrs(oa: {
-      version = "3.6.0";
-      src = pkgs.fetchurl {
-        url = "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.6.0/phpcs.phar";
-        sha256 = "0sdi78hrwd3r5p1b38qmp89m41kfszh2qn4n5zhq2dmhsjdhjziz";
-      };
-    }))
+    # Install phpcbf
+    php'.packages.phpcbf
+
+    # Install phpcs
+    php'.packages.phpcs
 
     # Install phpstan
     php'.packages.phpstan
 
     # Install yaml lint
     pkgs.yamllint
-
-    # Install nodejs for PHP LSP to work
-    pkgs.nodejs
   ];
 }
